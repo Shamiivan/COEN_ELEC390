f8a620c03daa0b867b9280a97a63e1fd590e0d4f
Cherry <60113072+Ch3rr13@users.noreply.github.com>
2023-11-16 13:51:57 -0500

    Modified Item options page

--- app/src/main/java/com/example/coenelec390/ui/item/Search.java
+++ app/src/main/java/com/example/coenelec390/ui/item/Search.java
@@ -4,19 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
+import com.example.coenelec390.R;
 import com.example.coenelec390.databinding.PageSearchBinding;
 
+import java.util.ArrayList;
+
 public class Search extends Fragment {
+    SearchView searchView;
+    ListView listView;
+
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
     private PageSearchBinding binding;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = PageSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        searchView = root.findViewById(R.id.searchBar);
+        listView = root.findViewById(R.id.searchlist);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("Item 1");
+        arrayList.add("Item 2");
+        arrayList.add("Item 3");
+        arrayList.add("Item 4");
+        arrayList.add("Item 5");
+        arrayList.add("Item 6");
+
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
         return root;
     }
 
