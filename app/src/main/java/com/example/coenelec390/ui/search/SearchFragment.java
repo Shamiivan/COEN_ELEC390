ae5c4abc58d24d74c6eaf8905ede48d77c67afc5
Cherry <60113072+Ch3rr13@users.noreply.github.com>
2023-11-28 18:13:08 -0500

    Modification on search list an bottom nav

--- app/src/main/java/com/example/coenelec390/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/coenelec390/ui/search/SearchFragment.java
@@ -106,25 +106,6 @@ public class SearchFragment extends Fragment  implements SearchAdapter.OnItemCli
                 return false;
             }
         });
-
-        /*Button viewCat = binding.viewCat;
-        viewCat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO
-            }
-        });*/
-
-        Button viewList = binding.viewList;
-        viewList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO
-            }
-        });
-
-
-
         return root;
     }
     void filter(String text){
@@ -139,12 +120,6 @@ public class SearchFragment extends Fragment  implements SearchAdapter.OnItemCli
                 temp.add(d);
 
             }
-        }
-
-        Utils.print("Update list ");
-        for (String name : temp
-             ) {
-                Utils.print(name);
         }
         // Update RecyclerView
         adapter.updateList(temp);
