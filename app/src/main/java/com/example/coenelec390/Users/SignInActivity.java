efd9e3a9411ba06d1cb11cd625f0c21d32e6d121
bashar9889 <bws9889@gmail.com>
2023-11-28 21:40:20 -0500

    signin java

---
+++ app/src/main/java/com/example/coenelec390/Users/SignInActivity.java
@@ -0,0 +1,76 @@
+package com.example.coenelec390.Users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.coenelec390.MainActivity;
+import com.example.coenelec390.R;
+import com.example.coenelec390.ui.dashboard.DashboardFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button signInButton = findViewById(R.id.buttonSignIn);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    signInUser(email, password);
+                } else {
+                    Toast.makeText(SignInActivity.this, "Please enter all details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView signUpPrompt = findViewById(R.id.signUpButton_question);
+        signUpPrompt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to start SignUpActivity
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void signInUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d("SignIn", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        // Redirect to another activity after successful login
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Log.w("SignIn", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/coenelec390/Users/SignUpActivity.java
@@ -0,0 +1,63 @@
+package com.example.coenelec390.Users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.coenelec390.R;
+import com.example.coenelec390.ui.dashboard.DashboardFragment;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button signUpButton = findViewById(R.id.buttonSignUp);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    createNewUser(email, password);
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Please enter all details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void createNewUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d("SignUp", "createUserWithEmail:success");
+                        // Redirect to another activity after successful sign-up
+                         Intent intent = new Intent(SignUpActivity.this, DashboardFragment.class);
+                         startActivity(intent);
+                    } else {
+                        Log.w("SignUp", "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
